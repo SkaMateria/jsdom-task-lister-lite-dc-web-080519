0f348758e9f99b6447d6f06b0b14072cfe528416
SkaMateria <skamateria@gmail.com>
2019-09-18 11:05:05 -0400

    put that span in when creating the dropdown and now it works sucka

--- src/index.js
+++ src/index.js
@@ -21,42 +21,36 @@ function delete123(e) {
 // "color selector" functionality
 function colorChange(event) {
   if(event.target.value == "Red"){
-    debugger
+    event.target.parentElement.querySelector("span").style.color = "red"
   }
   if(event.target.value == "Yellow"){
-    console.log("It's Yellow!")
+    event.target.parentElement.querySelector("span").style.color = "yellow"
   }
   if(event.target.value == "Green"){
-    console.log("It's GREEN, VICTOR! IT'S GREEN!!!")
+    event.target.parentElement.querySelector("span").style.color = "green"
   }
 
 
 
 }
-
+// everything that happens when you click Create New Task Button
 function buttonTest(event) {
   let newRow = document.createElement("li")
-  newRow.innerText = description.value
+  let taskDescriptionSpan = document.createElement("span")
+  taskDescriptionSpan.innerText = description.value
   let deleteButton = document.createElement("button")
   let coloredButton = document.createElement("select");
-  
+
+
+
   deleteButton.className = `${description.value}`;
-  deleteButton.innerText = "x"
-  newRow.appendChild(deleteButton)
-  newRow.appendChild(coloredButton);
+  deleteButton.innerText = "X"
+  newRow.append(taskDescriptionSpan, deleteButton, coloredButton)
+  // newRow.appendChild(coloredButton);
   tasks.appendChild(newRow)
 
 
-
-
-
-
-  
-  
-  
-  
-  
-  
+// drop selector
   for (var i = 0; i < colors.length; i++) {
     var option = document.createElement("option");
     option.setAttribute("value", colors[i]);
@@ -64,7 +58,7 @@ function buttonTest(event) {
     coloredButton.appendChild(option);
   }
   
-  
+  //event listeners for drop down and delete buttons
   coloredButton.addEventListener("change", colorChange)
   deleteButton.addEventListener("click", delete123)
     event.target.reset()
